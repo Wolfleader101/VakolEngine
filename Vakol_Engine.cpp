3099287bd622a30e6030b6e9d150cba49e4bf144
Wolfleader <james.morgan@iinet.net.au>
2023-03-02 13:04:21 +0800

    init

---
+++ Vakol_Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Vakol_Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Vakol_Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
