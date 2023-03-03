213bfcfea51e64d7e6992a9ef23db7337c12172d
Wolfleader <james.morgan@iinet.net.au>
2023-03-03 12:05:12 +0800

    adding in lua and assimp

--- Core/src/core.cpp
+++ Core/src/core.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <sol/sol.hpp>
 
 namespace Vakol {
 
 	void Main()
 	{
 		std::cout << "Hello from core" << std::endl;
+
+		sol::state lua;
+		lua.open_libraries(sol::lib::base);
+
+		lua.script("print('Hello from lua!')");
 	}
 }
