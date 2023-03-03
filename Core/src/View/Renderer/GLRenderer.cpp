8e1fb230fdaaa3498bdff14923293286ba9bb4d7
Wolfleader <james.morgan@iinet.net.au>
2023-03-03 21:09:44 +0800

    fixed issue

--- Core/src/View/Renderer/GLRenderer.cpp
+++ Core/src/View/Renderer/GLRenderer.cpp
@@ -1,6 +1,8 @@
-#include <View/Renderer/Renderer.hpp>
+#include "GLRenderer.hpp"
+
+#include <glad/glad.h>
 
 namespace Vakol::View {
-GLRenderer::GLRenderer(const std::shared_ptr<Window> window); : Renderer(window){};
-void GLRenderer::Update(const Controller::Time& time) override{};
+GLRenderer::GLRenderer(const std::shared_ptr<Window> window) : Renderer(window){};
+void GLRenderer::Update(const Controller::Time& time){};
 }  // namespace Vakol::View
