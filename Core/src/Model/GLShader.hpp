824fde4c2545be1b491171593f8eb81106fbb381
Caleb <cjmeepers@gmail.com>
2023-03-04 15:48:05 +0800

    Update GLShader.hpp

--- Core/src/Model/GLShader.hpp
+++ Core/src/Model/GLShader.hpp
@@ -6,21 +6,32 @@ namespace Vakol::Model
 {
 	class GLShader : public Shader
 	{
+        GLShader(const std::string& vertex, const std::string& fragment);
+        GLShader(const std::string& program);
+
 		~GLShader() override;
+
+        void Bind() const override;
+        void Unbind() const override;
+
+        unsigned int GetID() const;
 
         void SetBool(const std::string& name, const bool value) override;
         void SetInt(const std::string& name, const int value) override;
         void SetFloat(const std::string& name, const float value) override;
 
-        void SetFloat2(const std::string& name, const glm::vec2&) override;
+        void SetFloat2(const std::string& name, const glm::vec2& value) override;
         void SetFloat2(const std::string& name, const float x, const float y) override;
 
-        void SetFloat3(const std::string& name, const glm::vec3&) override;
+        void SetFloat3(const std::string& name, const glm::vec3& value) override;
         void SetFloat3(const std::string& name, const float x, const float y, const float z) override;
 
-        void SetFloat4(const std::string& name, const glm::vec4&) override;
+        void SetFloat4(const std::string& name, const glm::vec4& value) override;
 
-        void SetMat3(const std::string& name, const glm::mat3&) override;
-        void SetMat4(const std::string& name, const glm::mat4&) override;
+        void SetMat3(const std::string& name, const glm::mat3& value) override;
+        void SetMat4(const std::string& name, const glm::mat4& value) override;
+
+    private:
+        unsigned int id;
 	};
 }
