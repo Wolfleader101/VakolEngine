458f0a1e0eed5d272964a5f98bc6df46244ddea6
Caleb <cjmeepers@gmail.com>
2023-03-04 15:32:16 +0800

    add const qualifiers

--- Core/src/Model/Texture.hpp
+++ Core/src/Model/Texture.hpp
@@ -9,15 +9,15 @@ namespace Vakol::Model
 	{
 		virtual ~Texture() = default;
 
-		virtual void Bind() = 0;
+		virtual void Bind() const = 0;
 
-		virtual void Unbind() = 0;
+		virtual void Unbind() const = 0;
 
-		virtual unsigned int GetID() = 0;
+		virtual unsigned int GetID() const = 0;
 
-		//static std::shared_ptr<Texture> Create(const std::string& name)
-		//{
-		//	return std::make_shared<
-		//}
+		static std::shared_ptr<Texture> Create(const std::string& name)
+		{
+			return std::make_shared<GLTexture>(name);
+		}
 	};
 }
