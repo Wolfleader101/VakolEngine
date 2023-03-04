1fea6f4f7b82f01f7216f9a7e992c4b325f2abb8
Caleb <cjmeepers@gmail.com>
2023-03-04 13:22:52 +0800

    Update Texture.hpp

--- Core/src/Model/Texture.hpp
+++ Core/src/Model/Texture.hpp
@@ -3,17 +3,18 @@
 #include <string>
 #include <memory>
 
-namespace Vakol::Model {
+namespace Vakol::Model 
+{
+	struct Texture 
+	{
+		virtual ~Texture() = default;
 
-struct Texture {
-	virtual ~Texture() = default;
+		virtual void Bind() = 0;
 
-	virtual void Bind() = 0;
+		virtual void Unbind() = 0;
 
-	virtual void Unbind() = 0;
+		virtual unsigned int GetID() = 0;
 
-	virtual unsigned int GetID() = 0;
-
-	static std::shared_ptr<Texture> Create(const std::string& name);
-};
+		static std::shared_ptr<Texture> Create(const std::string& name);
+	};
 }
