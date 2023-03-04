4c96f15133e09828fb6748d49c591579c681373b
Caleb <cjmeepers@gmail.com>
2023-03-04 14:19:28 +0800

    add GLVBO, GLVAO, GLIBO

--- Core/src/Model/GLMesh.hpp
+++ Core/src/Model/GLMesh.hpp
@@ -4,13 +4,61 @@
 
 namespace Vakol::Model
 {
-	class GLVertexArray : public VertexArray
+	class GLVertexBuffer : public VertexBuffer
 	{
+	public:
+		GLVertexBuffer(const std::vector<Vertex>& vertices);
+		GLVertexBuffer(const std::vector<Vector3>& vertices);
 
+		~GLVertexBuffer() = default;
+
+		void Bind() override;
+		void Unbind() override;
+
+	private:
+		unsigned int id;
 	};
 
-	class GLMesh : public Mesh 
+	class GLIndexBuffer : public IndexBuffer
 	{
+	public:
+		GLIndexBuffer(const std::vector<unsigned int>& indices);
 
+		~GLIndexBuffer() = default;
+
+		void Bind() override;
+		void Unbind() override;
+
+	private:
+		unsigned int id;
+	};
+
+	class GLVertexArray : public VertexArray
+	{
+	public:
+		GLVertexArray(const Mesh& mesh);
+
+		~GLVertexArray() override;
+
+		void Bind() override;
+
+		void Unbind() override;
+
+	private:
+		unsigned int id;
+
+		std::shared_ptr<GLVertexBuffer> vertexBuffer;
+		std::shared_ptr<GLIndexBuffer> indexBuffer;
+	};
+
+	class GLMesh
+	{
+	public:
+		GLMesh() = default;
+
+	private:
+		GLVertexArray VAO;
+		GLVertexBuffer VBO;
+		GLIndexBuffer IBO;
 	};
 }
