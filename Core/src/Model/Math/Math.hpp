1df1e14952414f22c24f803e960509405d462cdf
Caleb <cjmeepers@gmail.com>
2023-03-04 16:03:22 +0800

    Update Math.hpp

--- Core/src/Model/Math/Math.hpp
+++ Core/src/Model/Math/Math.hpp
@@ -6,24 +6,24 @@
 
 #include <algorithm>
 #include <cmath>
+
 #include <glm/common.hpp>
 #include <glm/glm.hpp>
+
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-namespace Vakol::Model::Math {
-static inline bool NearEqual(const float a, const float b, const float epsilon) { return abs(a - b) < epsilon; }
-static inline float Clamp01(const float value) { return std::max(0.0f, std::min(value, 1.0f)); }
+namespace Vakol::Model::Math 
+{
+	static inline bool NearEqual(const float a, const float b, const float epsilon) { return abs(a - b) < epsilon; }
+	static inline float Clamp01(const float value) { return std::max(0.0f, std::min(value, 1.0f)); }
+	static inline float Clamp(const float value, const float lower, const float upper) {return std::max(lower, std::min(value, upper)); }
 
-static inline float Clamp(const float value, const float lower, const float upper) {
-    return std::max(lower, std::min(value, upper));
+	static const inline double PI = M_PI;
+
+	static const inline float FLOAT_MAX = FLT_MAX;
+	static const inline float FLOAT_MIN = FLT_MIN;
+
+	static const inline double Deg2Rad = PI / 180.0f;
+	static const inline double Rad2Deg = 180.0f / PI;
 }
-
-static const inline double PI = M_PI;
-
-static const inline float FLOAT_MAX = FLT_MAX;
-static const inline float FLOAT_MIN = FLT_MIN;
-
-static const inline float Deg2Rad = PI / 180.0f;
-static const inline float Rad2Deg = 180.0f / PI;
-}  // namespace Vakol::Model::Math
