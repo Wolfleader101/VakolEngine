fc38f148e0469de6223c66f5f8e0adc4cb1f0ae1
Caleb <cjmeepers@gmail.com>
2023-03-04 13:49:29 +0800

    Create Material.hpp

---
+++ Core/src/Model/Material.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+
+#include "Shader.hpp"
+#include "Texture.hpp"
+
+namespace Vakol::Model
+{
+	struct Material
+	{
+		Material() = default;
+
+		//std::vector<Texture> textures;
+	};
+}
