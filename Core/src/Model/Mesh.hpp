56de94b4464a37c74ba21428613e9c2389e78542
Caleb <cjmeepers@gmail.com>
2023-03-04 13:14:17 +0800

    add VAO, VBO, and IBO

--- Core/src/Model/Mesh.hpp
+++ Core/src/Model/Mesh.hpp
@@ -1,30 +1,65 @@
 #pragma once
 
 #include <vector>
+#include <memory>
 
 #include "Math/Vector2.hpp"
 #include "Math/Vector3.hpp"
 
-namespace Vakol::Model {
-struct Vertex {
-    Math::Vec3 position;
-    // ADD THIS LATER//Vector3 normal;
-    Math::Vec2 uv;
-};
+namespace Vakol::Model 
+{
+    struct Vertex 
+    {
+        Math::Vec3 position;
+        Math::Vec3 normal;
+        Math::Vec2 uv;
+    };
 
-struct Mesh {
-    Mesh() = default;
-    ~Mesh() { this->Clear(); }
+    struct Mesh 
+    {
+        Mesh() = default;
+        ~Mesh() { this->Clear(); }
 
-    void Clear() {
-        vertices.clear();
-        indices.clear();
-    }
+        void Clear() 
+        {
+            vertices.clear();
+            indices.clear();
+        }
 
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+    };
 
-   private:
-    unsigned int id;
-};
-}  // namespace Vakol::Model
+    struct VertexArrayBuffer
+    {
+        virtual ~VertexArrayBuffer() = default;
+
+        virtual void Bind() = 0;
+
+        virtual void Unbind() = 0;
+
+        static std::shared_ptr<VertexArrayBuffer> Create(const Mesh& mesh);
+    };
+
+    struct VertexBuffer
+    {
+        virtual ~VertexBuffer() = default;
+
+        virtual void Bind() = 0;
+
+        [[maybe_unused]] virtual void Unbind() = 0;
+
+        static std::shared_ptr<VertexBuffer> Create(const std::vector<Vertex>& vertices);
+    };
+
+    struct IndexBuffer
+    {
+        virtual ~IndexBuffer() = default;
+
+        virtual void Bind() = 0;
+
+        virtual void Unbind() = 0;
+
+        static std::shared_ptr<IndexBuffer> Create(const std::vector<unsigned int>& indices);
+    };
+}
