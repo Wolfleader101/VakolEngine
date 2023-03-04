ab86bc6e4c231b1a5ef4a1f9492e1120d5c10076
Caleb <cjmeepers@gmail.com>
2023-03-04 19:05:07 +0800

    trying to draw a triangle

--- Core/src/View/Renderer/GLRenderer.cpp
+++ Core/src/View/Renderer/GLRenderer.cpp
@@ -2,13 +2,41 @@
 
 #include <glad/glad.h>
 
-namespace Vakol::View {
-GLRenderer::GLRenderer(const std::shared_ptr<Window> window) : Renderer(window) {
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_2D);
-};
-void GLRenderer::Update(const Controller::Time& time) {
-    glClearColor(0.52941f, 0.80784f, 0.92157f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-};
-}  // namespace Vakol::View
+#include "Model/GLMesh.hpp"
+#include "Model/GLShader.hpp"
+
+using Vakol::Model::Shader;
+
+using Vakol::Model::VertexArray;
+using Vakol::Model::Vertex;
+
+using Vakol::Model::Math::Vec3;
+using Vakol::Model::Math::Vec2;
+
+namespace Vakol::View 
+{
+    GLRenderer::GLRenderer(const std::shared_ptr<Window> window) : Renderer(window) 
+    {
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_TEXTURE_2D);
+
+        auto shader = Shader::Create("basic.vert", "basic.frag");
+
+        std::vector<Vertex> vertices =
+        {
+            {Vec3(0.5f, -0.5f, 0.0f), Vec3(0.0f, 0.0f, 0.0f), Vec2(0.0f, 0.0f)},
+            {Vec3(-0.5f, -0.5f, 0.0f), Vec3(0.0f, 0.0f, 0.0f), Vec2(1.0f, 0.0f)},
+            {Vec3(0.0f, 0.5f, 0.0f),    Vec3(0.0f, 0.0f, 0.0f), Vec2(0.5f, 1.0f)}
+        };
+
+        std::vector<unsigned int> indices = { 0, 1, 3 };
+
+        auto vao = VertexArray::Create(vertices, indices);
+    };
+
+    void GLRenderer::Update(const Controller::Time& time) 
+    {
+        glClearColor(0.52941f, 0.80784f, 0.92157f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    };
+}
