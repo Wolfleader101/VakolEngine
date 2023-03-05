48c70d89022ec56c1ae306ae8fc2d519f685cea3
Caleb <cjmeepers@gmail.com>
2023-03-05 22:01:01 +0800

    Update GLRenderer.hpp

--- Core/src/View/Renderer/GLRenderer.hpp
+++ Core/src/View/Renderer/GLRenderer.hpp
@@ -2,10 +2,12 @@
 
 #include "Renderer.hpp"
 
-namespace Vakol::View {
-class GLRenderer : public Renderer {
-   public:
-    GLRenderer(const std::shared_ptr<Window> window);
-    void Update(const Controller::Time& time) override;
-};
-}  // namespace Vakol::View
+namespace Vakol::View 
+{
+    class GLRenderer : public Renderer 
+    {
+    public:
+        GLRenderer(const std::shared_ptr<Window> window);
+        void Update(const Controller::Time& time) override;
+    };
+}
