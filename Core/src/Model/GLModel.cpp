3425eb67a916bb6864a5bb84a02017709b1c4c16
Caleb <cjmeepers@gmail.com>
2023-03-05 23:51:29 +0800

    Update GLModel.cpp

--- Core/src/Model/GLModel.cpp
+++ Core/src/Model/GLModel.cpp
@@ -1,19 +1,50 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <vector>
 
 #include "GLModel.hpp"
 
 namespace Vakol::Model
 {
+	GLMesh::GLMesh(const std::shared_ptr<GLVertexArray>& VAO)
+	{
+		this->VAO = VAO;
+	}
+
+	GLMesh::~GLMesh()
+	{
+		this->VAO->~GLVertexArray();
+	}
+
+	void GLMesh::Draw() const
+	{
+		this->VAO->Bind();
+		glDrawArrays(GL_TRIANGLES, 0, this->VAO->GetVertexCount());
+		this->VAO->Unbind();
+	}
+
 	GLModel::~GLModel()
 	{
-
+		for (const auto& it : meshes)
+		{
+			it.first.~GLMesh();
+			it.second.~GLMaterial();
+		}
 	}
 
 	void GLModel::Draw() const
 	{
-		for (int i = 0; i < meshes.size(); ++i)
+		for (const auto& it : meshes)
 		{
+			it.second.Bind();
 
+			it.first.VAO->Bind();
 		}
+	}
+
+	void GLModel::AddMesh(const std::shared_ptr<GLVertexArray>& VAO, const std::string& name)
+	{
+		meshes.emplace(std::make_shared<GLMesh>(VAO), std::make_shared<GLMaterial>(name));
 	}
 }
