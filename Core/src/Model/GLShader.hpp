642cc543118e1e5c49ddc4fded60b2c42c115a11
Caleb <cjmeepers@gmail.com>
2023-03-05 21:22:00 +0800

    Remove the SetValues from Shader to Material

--- Core/src/Model/GLShader.hpp
+++ Core/src/Model/GLShader.hpp
@@ -17,24 +17,8 @@ namespace Vakol::Model
 
         unsigned int GetID() const;
 
-        void SetBool(const std::string& name, const bool value) override;
-        void SetInt(const std::string& name, const int value) override;
-        void SetFloat(const std::string& name, const float value) override;
-
-        void SetFloat2(const std::string& name, const glm::vec2& value) override;
-        void SetFloat2(const std::string& name, const float x, const float y) override;
-
-        void SetFloat3(const std::string& name, const glm::vec3& value) override;
-        void SetFloat3(const std::string& name, const float x, const float y, const float z) override;
-
-        void SetFloat4(const std::string& name, const glm::vec4& value) override;
-
-        void SetMat3(const std::string& name, const glm::mat3& value) override;
-        void SetMat4(const std::string& name, const glm::mat4& value) override;
-
     private:
-        unsigned int id;
-
+        unsigned int id = 0;
     private:
         std::string ReadFile(const std::string& name);
         void CreateProgram(const std::string& vertex, const std::string& fragment);
--- Core/src/Model/Material.hpp
+++ Core/src/Model/Material.hpp
@@ -1,16 +1,28 @@
 #pragma once
 
-#include <vector>
-
-#include "Shader.hpp"
-#include "Texture.hpp"
-
 namespace Vakol::Model
 {
-	struct Material
+	class Material
 	{
-		Material() = default;
+	public:
+		virtual ~Material() = default;
 
-		//std::vector<Texture> textures;
+		virtual void Bind() const = 0;
+		virtual void Unbind() const = 0;
+
+        virtual void SetBool(const std::string& name, const bool value) = 0;
+        virtual void SetInt(const std::string& name, const int value) = 0;
+        virtual void SetFloat(const std::string& name, const float value) = 0;
+
+        virtual void SetFloat2(const std::string& name, const glm::vec2& value) = 0;
+        virtual void SetFloat2(const std::string& name, const float x, const float y) = 0;
+
+        virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
+        virtual void SetFloat3(const std::string& name, const float x, const float y, const float z) = 0;
+
+        virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
+
+        virtual void SetMat3(const std::string& name, const glm::mat3& value) = 0;
+        virtual void SetMat4(const std::string& name, const glm::mat4& value) = 0;
 	};
 }
