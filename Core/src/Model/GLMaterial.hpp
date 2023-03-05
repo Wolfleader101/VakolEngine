9320f5dbf4eebcf04021d1aa3ca3ca6b350842b1
Caleb <cjmeepers@gmail.com>
2023-03-05 21:21:27 +0800

    Update GLMaterial.hpp

--- Core/src/Model/GLMaterial.hpp
+++ Core/src/Model/GLMaterial.hpp
@@ -1,1 +1,41 @@
 #pragma once
+
+#include <memory>
+#include <vector>
+
+#include "Material.hpp"
+
+#include "GLShader.hpp"
+#include "GLTexture.hpp"
+
+namespace Vakol::Model
+{
+	class GLMaterial : public Material
+	{
+		GLMaterial(const GLShader& shader);
+		~GLMaterial() override;
+
+		void Bind() const override;
+		void Unbind() const override;
+
+		void AddTexture(const GLTexture& texture);
+
+		void SetBool(const std::string& name, const bool value) override;
+		void SetInt(const std::string& name, const int value) override;
+		void SetFloat(const std::string& name, const float value) override;
+
+		void SetFloat2(const std::string& name, const glm::vec2& value) override;
+		void SetFloat2(const std::string& name, const float x, const float y) override;
+
+		void SetFloat3(const std::string& name, const glm::vec3& value) override;
+		void SetFloat3(const std::string& name, const float x, const float y, const float z) override;
+
+		void SetFloat4(const std::string& name, const glm::vec4& value) override;
+
+		void SetMat3(const std::string& name, const glm::mat3& value) override;
+		void SetMat4(const std::string& name, const glm::mat4& value) override;
+	private:
+		std::vector<std::shared_ptr<GLTexture>> textures;
+		std::shared_ptr<GLShader> shader;
+	};
+}
