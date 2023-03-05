97b721a2dce20296e903a4a76fec73d29f286164
Caleb <cjmeepers@gmail.com>
2023-03-05 22:10:12 +0800

    clearing out the clutter

--- Core/src/View/Renderer/GLRenderer.cpp
+++ Core/src/View/Renderer/GLRenderer.cpp
@@ -2,16 +2,9 @@
 
 #include <glad/glad.h>
 
-#include "Model/GLMesh.hpp"
 #include "Model/GLShader.hpp"
 
 using Vakol::Model::GLShader;
-
-using Vakol::Model::GLVertexArray;
-using Vakol::Model::Vertex;
-
-using Vakol::Model::Math::Vec3;
-using Vakol::Model::Math::Vec2;
 
 namespace Vakol::View 
 {
@@ -21,17 +14,6 @@ namespace Vakol::View
         glEnable(GL_TEXTURE_2D);
 
         auto shader = std::make_shared<GLShader>("basic.vert", "basic.frag");
-
-        std::vector<Vertex> vertices =
-        {
-            {Vec3(0.5f, -0.5f, 0.0f),   Vec3(0.0f, 0.0f, 0.0f), Vec2(0.0f, 0.0f)},
-            {Vec3(-0.5f, -0.5f, 0.0f),  Vec3(0.0f, 0.0f, 0.0f), Vec2(1.0f, 0.0f)},
-            {Vec3(0.0f, 0.5f, 0.0f),    Vec3(0.0f, 0.0f, 0.0f), Vec2(0.5f, 1.0f)}
-        };
-
-        std::vector<unsigned int> indices = { 0, 1, 3 };
-
-        auto vao = std::make_shared<GLVertexArray>(vertices, indices);
     };
 
     void GLRenderer::Update(const Controller::Time& time) 
