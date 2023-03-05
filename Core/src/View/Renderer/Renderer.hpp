00315b6e5578f9a11f1dcc486bfa222b845818ff
Caleb <cjmeepers@gmail.com>
2023-03-05 22:00:47 +0800

    Update Renderer.hpp

--- Core/src/View/Renderer/Renderer.hpp
+++ Core/src/View/Renderer/Renderer.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
-#include <Controller/Time.hpp>
-#include <View/Window/Window.hpp>
 #include <memory>
 
-namespace Vakol::View {
-class Renderer {
-   public:
-    Renderer(const std::shared_ptr<Window> window) : m_window(window){};
-    virtual ~Renderer() {}
+#include <Controller/Time.hpp>
+#include <View/Window/Window.hpp>
 
-    virtual void Update(const Controller::Time& time) = 0;
+namespace Vakol::View 
+{
+    class Renderer 
+    {
+    public:
+        Renderer(const std::shared_ptr<Window> window) : m_window(window){};
+        virtual ~Renderer() {}
 
-   protected:
-    std::shared_ptr<Window> m_window;
-};
-}  // namespace Vakol::View
+        virtual void Update(const Controller::Time& time) = 0;
+
+    protected:
+        std::shared_ptr<Window> m_window;
+    };
+}
