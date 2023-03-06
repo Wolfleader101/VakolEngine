1c7c6d9d5563948c05d19253924e3091fa3dd7e0
Wolfleader <james.morgan@iinet.net.au>
2023-03-06 14:57:32 +0800

    Set up proper includes to hide core functionality

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -1,14 +1,11 @@
 ﻿// Vakol_Engine.cpp : Defines the entry point for the application.
 //
 
-#include <Controller/Logger.hpp>
-#include <Controller/LuaState.hpp>
-#include <Controller/Singleton.hpp>
-#include <core.hpp>
 #include <iostream>
 
+#include "Vakol/core.hpp"
+
 int main() {
-    Vakol::Controller::Singleton<Vakol::Controller::LuaState>::GetInstance().RunFile("assets/test.lua");
     Vakol::Run();
     return 0;
 }
---
+++ Demo/scripts/game_config.lua
@@ -0,0 +1,6 @@
+local config = {
+    name = "Vakol Demo",
+    window_height = 720,
+    window_width = 1280,
+    renderer = "gl"
+}
