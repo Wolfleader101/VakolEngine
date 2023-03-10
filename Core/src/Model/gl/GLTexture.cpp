294bce82a6d2d0c9e11f58968f58cca2b34915cd
Caleb <cjmeepers@gmail.com>
2023-03-10 19:23:17 +0800

    add texture function and remove shaderlibrary comments

--- Core/src/Model/gl/GLTexture.cpp
+++ Core/src/Model/gl/GLTexture.cpp
@@ -5,6 +5,36 @@
 
 namespace Vakol::Model
 {
+	unsigned int GLTexture::GetTextureCubemap(const std::vector<std::string>& faces)
+	{
+		unsigned int textureID;
+
+		glGenTextures(1, &textureID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+		int width, height, colorDepth;
+		
+		for (auto i = 0; i < faces.size(); i++)
+		{
+			unsigned char* data = Texture::GetImage(faces[i], width, height, colorDepth);
+
+			GLenum format = (colorDepth > 3) ? GL_RGBA : GL_RGB;
+
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+
+			Texture::FreeImage(data);
+		}
+
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_REPEAT);
+
+		return textureID;
+	}
+
 	unsigned int GLTexture::GetTexture(const std::string& path)
 	{
 		unsigned int textureID;
--- Core/src/Model/gl/GLTexture.hpp
+++ Core/src/Model/gl/GLTexture.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include "../Texture.hpp"
 
 namespace Vakol::Model
@@ -7,6 +9,7 @@ namespace Vakol::Model
 	class GLTexture
 	{
 	public:
+		static unsigned int GetTextureCubemap(const std::vector<std::string>& faces);
 		static unsigned int GetTexture(const std::string& path);
 	};
 }
