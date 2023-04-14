f5981d0bf853488cdb56621e9b4b90d95998141f
Wolfleader <james.morgan@iinet.net.au>
2023-04-14 09:31:32 +0800

    obj fix

--- Demo/scripts/testScene.lua
+++ Demo/scripts/testScene.lua
@@ -1,7 +1,6 @@
 function init(scene)
     scene:create_entity("cameraController.lua");
-    entity = scene:create_entity("player.lua");
-    transform = entity:get_transform();
+    scene:create_entity("player.lua");
 end
 
 
