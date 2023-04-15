1ecb22576d7f8a3bc704730692de99989d0c0dbc
Caleb <cjmeepers@gmail.com>
2023-04-15 15:32:47 +0800

    Update GLInstance.cpp

--- Core/src/Model/gl/GLInstance.cpp
+++ Core/src/Model/gl/GLInstance.cpp
@@ -14,6 +14,18 @@ void CreateInstanced(const std::vector<Mesh>& meshes, const std::vector<glm::mat
         unsigned int vao = meshes.at(i).vao()->GetID();
         glBindVertexArray(vao);
 
+        // mat4 is a 4x4 matrix (4 * vec4), which in our case, represents the transformation matrix of a model.
+        // transformation matrix containing the translation, scaling, and rotation of a model.
+
+        // since the max size of an attribute in glsl shaders is a vec4,
+        // we need to reserve enough space for a mat4
+
+        // sizeof(mat4) = 4 * sizeof(vec4) = 16 * sizeof(float)
+
+        // vec4 is a 4x1 vector representing 4 floats
+
+        // set the attribute location to 3, reserving 4 GL_FLOATS in the vertex array
+
         glEnableVertexAttribArray(3);
         glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
 
