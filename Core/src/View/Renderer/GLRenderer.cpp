3c0d59ce31c21d2b2be35b55d8cba03be98583cb
Wolfleader <james.morgan@iinet.net.au>
2023-04-21 09:18:33 +0800

    fixed bug with shaders

--- Core/src/View/Renderer/GLRenderer.cpp
+++ Core/src/View/Renderer/GLRenderer.cpp
@@ -2,16 +2,14 @@
 
 #include <glad/glad.h>
 
-#include <vector>
-#include <memory>
-
 #include <Controller/Logger.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <memory>
+#include <vector>
 
 #include "Model/Components.hpp"
 #include "Model/gl/GLUniformBuffer.hpp"
-
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 /*
 Distance	Constant	Linear	Quadratic
@@ -62,13 +60,12 @@ const float light_cut_off = glm::cos(glm::radians(7.5f));
 const float light_outer_cut_off = glm::cos(glm::radians(12.5f));
 
 namespace Vakol::View {
-    GLRenderer::GLRenderer(const std::shared_ptr<Window> window) : Renderer(window) 
-    {
+    GLRenderer::GLRenderer(const std::shared_ptr<Window> window) : Renderer(window) {
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_CULL_FACE);
 
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -84,24 +81,27 @@ namespace Vakol::View {
         // SetUniformData(1, 4 * sizeof(float), sizeof(float), &light_outer_cut_off);
     };
 
-    void GLRenderer::AddUniform(const int size, const int binding)
-	{
-		this->m_uniforms.push_back(std::make_shared<GLUniformBuffer>(size, binding));
-	}
+    void GLRenderer::AddUniform(const int size, const int binding) {
+        this->m_uniforms.push_back(std::make_shared<GLUniformBuffer>(size, binding));
+    }
 
-	void GLRenderer::SetUniformData(const int index, const int offset, const int size, const void* data) const
-	{
-		if (this->m_uniforms[index]) this->m_uniforms[index]->SetData(offset, size, data);
-		else VK_ERROR("Uniform buffer at index {0} does not exist!", index);
-	}
+    void GLRenderer::SetUniformData(const int index, const int offset, const int size, const void* data) const {
+        if (this->m_uniforms[index])
+            this->m_uniforms[index]->SetData(offset, size, data);
+        else
+            VK_ERROR("Uniform buffer at index {0} does not exist!", index);
+    }
 
     void GLRenderer::ClearColor(const glm::vec4& color) const { glClearColor(color.r, color.g, color.b, color.a); }
-    void GLRenderer::ClearColor(const float r, const float g, const float b, const float a) const { glClearColor(r,g,b,a); }
+    void GLRenderer::ClearColor(const float r, const float g, const float b, const float a) const {
+        glClearColor(r, g, b, a);
+    }
 
     void GLRenderer::ClearBuffer(const unsigned int buffer_bit) const { glClear(buffer_bit); }
 
-    void GLRenderer::Draw(const Controller::Time& time, const Controller::Camera& camera, const Model::Components::Transform trans, const Model::Components::Drawable& drawable) const 
-    {
+    void GLRenderer::Draw(const Controller::Time& time, const Controller::Camera& camera,
+                          const Model::Components::Transform trans, const Model::Components::Drawable& drawable) const {
+        drawable.model_ptr->GetShader()->Bind();
         SetUniformData(0, 0, sizeof(glm::mat4), glm::value_ptr(camera.GetMatrix(PV_MATRIX)));
         SetUniformData(0, sizeof(glm::mat4), sizeof(glm::mat4), glm::value_ptr(camera.GetMatrix(VIEW_MATRIX)));
 
@@ -118,19 +118,18 @@ namespace Vakol::View {
         model_matrix = glm::rotate(model_matrix, trans.rot.z, glm::vec3(0.0f, 0.0f, 1.0f));
 
         drawable.model_ptr->GetShader()->SetMat4("MODEL_MATRIX", model_matrix);
-        // drawable.model_ptr->GetShader()->SetMat3("NORMAL_MATRIX", glm::transpose(glm::inverse(glm::mat3(model_matrix))));
+        // drawable.model_ptr->GetShader()->SetMat3("NORMAL_MATRIX",
+        // glm::transpose(glm::inverse(glm::mat3(model_matrix))));
 
         // drawable.model_ptr->GetShader()->SetVec3v("VIEW_POS", camera.GetPos());
 
         // drawable.model_ptr->GetShader()->SetVec3v("light.position", camera.GetPos());
         // drawable.model_ptr->GetShader()->SetVec3v("light.direction", camera.GetForward());
 
-        for (int i = 0; i < drawable.model_ptr->GetMeshCount(); ++i)
-        {   
-            auto mesh  = drawable.model_ptr->GetMeshes().at(i);
-            
-            switch (mesh.GetVertexArray()->GetDrawMode())
-            {
+        for (int i = 0; i < drawable.model_ptr->GetMeshCount(); ++i) {
+            auto mesh = drawable.model_ptr->GetMeshes().at(i);
+
+            switch (mesh.GetVertexArray()->GetDrawMode()) {
                 case ARRAYS:
                     mesh.GetVertexArray()->DrawArrays();
                     break;
@@ -153,11 +152,11 @@ namespace Vakol::View {
                     break;
             }
         }
+        drawable.model_ptr->GetShader()->Unbind();
     }
 
-    void GLRenderer::Update() const
-    {
+    void GLRenderer::Update() const {
         ClearColor(VAKOL_CLASSIC);
         ClearBuffer(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
-}
+}  // namespace Vakol::View
