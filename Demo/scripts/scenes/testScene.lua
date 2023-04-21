714fb7a188d54e871d045d94719bdd5d12acc3cd
Caleb <cjmeepers@gmail.com>
2023-04-21 23:45:35 +0800

    add kiki

--- Demo/scripts/scenes/testScene.lua
+++ Demo/scripts/scenes/testScene.lua
@@ -3,39 +3,41 @@ function init(scene)
     scene:create_entity("terrain", "entities/terrain.lua"); -- switch between basic or advanced_example
     scene:create_entity("Camera Controller", "entities/cameraController.lua");
 
-    for i=1,100 do
-        local cube = scene:create_entity("spinning Cube " .. i, "");
-        cube:get_transform().scale.x = 5;
-        cube:get_transform().scale.y = 5;
-        cube:get_transform().scale.z = 5;
+    for i=1,30 do
+        local kiki = scene:create_entity("kiki_model " .. i, "");
+
+        kiki:get_transform().scale.x = 5;
+        kiki:get_transform().scale.y = 5;
+        kiki:get_transform().scale.z = 5;
+
         local terr_ent = scene:get_entity("terrain");
         local terrain = terr_ent:get_terrain();
         local terr_scale = terr_ent:get_transform().scale;
+
         local x = math.random(0, terrain:get_size()) * 10;
         local z = math.random(0, terrain:get_size()) * 10;
-
         local y = (terrain:get_height(x / terr_scale.x, z / terr_scale.z) * terr_scale.y) + 5;
 
-        cube:get_transform().pos.x = x;
-        cube:get_transform().pos.y = y;
-        cube:get_transform().pos.z = z;
+        kiki:get_transform().pos.x = x;
+        kiki:get_transform().pos.y = y;
+        kiki:get_transform().pos.z = z;
 
-        local model = cube:add_model("coreAssets/models/cube.obj") -- get model and add a drawable component
+        local _model = kiki:add_model("coreAssets/models/kiki_resized.obj") -- get model and add a drawable component
 
-        local mesh = model:get_mesh();
+        local _mesh = _model:get_mesh();
 
-        local material = mesh:get_material();
+        local _material = _mesh:get_material();
 
-        model:set_shader("coreAssets/shaders/basic.prog") -- set the shader on the model (automatically binds it)
+        _model:set_shader("coreAssets/shaders/basic.prog") -- set the shader on the model (automatically binds it)
 
-        local diffuse_map = texture("coreAssets/textures/pisikek.png", false, false);
+        local _diffuse = texture("coreAssets/textures/kiki_body.jpg", false, false);
 
-        material:add_texture(diffuse_map);
-        diffuse_map:bind_texture(99);
+        _material:add_texture(_diffuse);
+        _diffuse:bind_texture(97);
 
 
-        local shader = model:get_shader(); -- get the shader from the model
-        shader:set_int("texture_diffuse", 99); -- in this case, we set a uniform variable "tint" of type vec3 to red
+        local shader = _model:get_shader(); -- get the shader from the model
+        shader:set_int("texture_diffuse", 97); -- in this case, we set a uniform variable "tint" of type vec3 to red
 
     end
 end
