9b63942ac20039a9207e93ac40c2cd97d116b5d6
Wolfleader <25214835+Wolfleader101@users.noreply.github.com>
2023-06-08 14:38:21 +0800

    Skeleton ai tea 178 (#63)
    * Skeleton AI Working
    
    AI is working, now will swap out the Deer model with the zombie model from Caleb
    
    * Changed File Name + Swapped Model
    
    Changed the name of the file to better reflect the model
    
    Swapped out the test deer model with the final model
    
    * Trying to access the state of a monster
    
    Trying to access a monsters state but having some issues
    
    * Monster Attacking Player Affects Others
    
    It works to switch the state of the other monsters, but I have yet to figure out how to get them to run over to me
    
    * alerted state
    
    ---------
    
    Co-authored-by: Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>

--- Demo/scripts/scenes/testScene.lua
+++ Demo/scripts/scenes/testScene.lua
@@ -6,24 +6,32 @@ function init()
     
     -- scene:create_entity("Bob", "entities/agents/bob.lua");
     -- scene:create_entity("Rabbit", "entities/agents/rabbit.lua");
-    scene:create_entity("Deer", "entities/agents/deer.lua");
+    -- scene:create_entity("Deer", "entities/agents/deer.lua");
 
-    --scene:create_entity("Rabbit", "entities/agents/rabbit.lua");
+    -- scene:create_entity("Rabbit", "entities/agents/rabbit.lua");
 
-    --scene:create_entity("Rabbit Test", "entities/agents/rabbit_test.lua");
+    -- scene:create_entity("Monster", "entities/agents/monster.lua");
 
-    local rabbits = {};
+    scene.globals.monsters = {};
     
     for i = 1, 5 do
-        rabbits[i] = scene:create_entity("Rabbit" .. i, "entities/agents/rabbit.lua");
-        rabbits[i]:get_transform().pos = Vector3.new(5.0 * i, 0.0, -2.0);
+        scene.globals.monsters[i] = scene:create_entity("Monster" .. i, "entities/agents/monster.lua");
+        scene.globals.monsters[i]:get_transform().pos = Vector3.new(5.0 * i, 0.0, -2.0);
     end
+
+    -- scene:create_entity("Rabbit Test", "entities/agents/rabbit_test.lua");
+
+    -- local rabbits = {};
+    
+    -- for i = 1, 5 do
+        -- rabbits[i] = scene:create_entity("Rabbit" .. i, "entities/agents/rabbit.lua");
+        -- rabbits[i]:get_transform().pos = Vector3.new(5.0 * i, 0.0, -2.0);
+    -- end
 
     -- scene:create_entity("Bears", "entities/agents/bear.lua")
     -- scene:create_entity("Rabbits", "entities/agents/rabbit.lua");
     -- scene:create_entity("Deers", "entities/agents/deer.lua");
     -- scene:create_entity("Wolves", "entities/agents/wolf.lua");
-
 end
 
 function update()
