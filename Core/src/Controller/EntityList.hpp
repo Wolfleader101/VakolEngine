d3c502d79dca6ede06c8f94b0240ca7c7b92aa15
Blake C <blake.caldwell5@gmail.com>
2023-06-09 15:34:07 +0800

    saving progress

--- Core/src/Controller/EntityList.hpp
+++ Core/src/Controller/EntityList.hpp
@@ -24,88 +24,112 @@ namespace Vakol::Controller {
     /**
      * @class EntityList
      *
-     *
-     * @brief Wrapper for entt registry and contains a list of all active entities
-     *
+     * @brief Wrapper for entt registry and contains a list of all active entities.
      */
     class EntityList {
        public:
         /**
-         * @brief Construct a new Entity Manager object
-         *
+         * @brief Construct a new Entity Manager object.
          */
         EntityList() : m_Registry(), ActiveEntityList(){};
 
         /**
-         * @brief Create an Entity object
+         * @brief Create an Entity object.
          *
-         * @return Entity created
+         * @return Entity created.
          */
         Entity CreateEntity();
 
         /**
-         * @brief Create an Entity object
+         * @brief Create an Entity object.
          *
-         * @param SuggestedHandle handle to use for entity
-         * @return Entity created
+         * @param SuggestedHandle Handle to use for entity.
+         * @return Entity created.
          */
         Entity CreateEntity(uint32_t SuggestedHandle);
 
         /**
-         * @brief Get an Entity object if it exists
+         * @brief Get an Entity object if it exists.
          *
-         * @param Handle to get
-         * @return Entity from list
+         * @param Handle Handle to get.
+         * @return Entity from list.
          */
         Entity GetEntity(uint32_t Handle);
 
         /**
-         * @brief Removes entity if it exists
+         * @brief Removes an entity if it exists.
          *
-         * @param Handle to remove
+         * @param Handle Handle to remove.
          */
-        void RemoveEntity(uint32_t Handle);  // can also use entity because type conversion provided in entity
+        void RemoveEntity(uint32_t Handle);  // can also use entity because type conversion is provided in entity
 
         /**
-         * @brief clears list and registry of all entities
-         *
+         * @brief Clears the list and registry of all entities.
          */
         void Clear();
 
         /**
-         * @brief Checks registry for existing entity handle
+         * @brief Checks the registry for the existence of an entity handle.
          *
+         * @param Handle Handle to check.
+         * @return bool True if the entity exists, false otherwise.
          */
         bool CheckEntityExistence(uint32_t Handle) const;
 
         /**
-         * @brief Get reference to the Entity List object
+         * @brief Get a reference to the Entity List object.
          *
-         * @return std::list<Entity>&
+         * @return std::vector<Entity>& Reference to the entity vector.
          */
         std::vector<Entity>& GetEntityVec();
 
         //-------------- THESE ARE ADDITIONS IN ICT397 ------------------
 
+        /**
+         * @brief Initialize the entity list.
+         */
         void Init();
 
+        /**
+         * @brief Update the entity list.
+         *
+         * @param d_t The time delta for the update.
+         */
         void Update(double d_t);
 
+        /**
+         * @brief Serialize the entity list to a file.
+         *
+         * @param file The file path to serialize to.
+         */
         void Serialize(const std::string& file) const;
+
+        /**
+         * @brief Deserialize the entity list from a file.
+         *
+         * @param file The file path to deserialize from.
+         */
         void Deserialize(const std::string& file);
 
        private:
         /**
-         * @brief EnTT registry actually containing all the entity and comp data
+         * @brief EnTT registry actually containing all the entity and component data.
          */
         entt::registry m_Registry;
 
         /**
-         * @brief list of entities created and active
+         * @brief List of entities created and active.
          */
         std::vector<Entity> ActiveEntityList;
 
         template <typename Archive, typename... Args>
+        /**
+         * @brief Private function to serialize the entity list using a specified archive and component types.
+         *
+         * @tparam Archive The archive type.
+         * @tparam Args The component types to serialize.
+         * @param file The file path to serialize to.
+         */
         void privateSerialize(const std::string& file) const {
             std::ofstream out(file);
             if (out.good()) {
@@ -123,16 +147,21 @@ namespace Vakol::Controller {
         }
 
         template <typename Archive, typename... Args>
+        /**
+         * @brief Private function to deserialize the entity list using a specified archive and component types.
+         *
+         * @tparam Archive The archive type.
+         * @tparam Args The component types to deserialize.
+         * @param file The file path to deserialize from.
+         */
         void privateDeserialize(const std::string& file) {
             std::ifstream inp(file);
 
-            if (inp.good()) 
-            {
+            if (inp.good()) {
                 m_Registry.clear();
                 Archive json(inp);
                 json(ActiveEntityList);  // fills vector again
 
-                
                 entt::snapshot_loader snapLoad(m_Registry);
                 snapLoad.entities(json);
                 snapLoad.component<Args...>(json);
@@ -141,66 +170,7 @@ namespace Vakol::Controller {
             }
         }
 
-        // template <typename Archive, typename... Args>
-        // void privateDeserialize(const std::string& file, std::shared_ptr<LuaState> lua, Scene* scene) {
-        //     std::ifstream inp(file);
-
-        //     if (inp.good()) {
-                
-
-        //         m_Registry.clear();
-        //         ActiveEntityList.clear();
-
-        //         Archive json(inp);
-        //         json(ActiveEntityList);  // fills vector again
-                
-                
-        //         entt::snapshot_loader snapLoad(m_Registry);
-        //         snapLoad.entities(json);
-        //         snapLoad.component<Args...>(json);
-
-
-
-        //         System::Script_Init(lua, *this, scene);
-
-        //         //inp.close();
-
-
-        //         inp.seekg(0, inp.beg); //reset file pointer to start of file
-
-        //         entt::registry tempReg;
-        //         entt::snapshot_loader tempSnap(tempReg);
-        //         tempSnap.entities(json);
-        //         tempSnap.component<Args...>(json);
-
-        //         //do n^2 view here
-                
-        //         m_Registry.group<Components::Script, Components::Transform, Components::GUID, Components::Tag>().each(
-        //             [&](auto& script, auto& trans, auto& guid, auto& tag)
-        //             {
-        //                     tempReg.group<Components::Script, Components::Transform, Components::GUID, Components::Tag>().each(
-        //                     [&](auto& scriptTemp, auto& transTemp, auto& guidTemp, auto& tagTemp)
-        //                     {
-        //                         if(guid == guidTemp)
-        //                         {
-        //                             Controller::ConvertMapToSol(scriptTemp.data, script.state); //convert map to sol state (lua
-        //                             script.state = scriptTemp.state;
-
-        //                             trans = transTemp;
-
-        //                             tag = tagTemp;
-        //                         }
-        //                     });
-        //             });
-
-                
-        //         inp.close();
-        //     }
-
-        //     
-        // }
-
-        friend class Entity;  // friend to allow the api for entities to be clean.
+        friend class Entity;  // friend to allow the API for entities to be clean.
         friend class System;
         friend class Scene;
     };
