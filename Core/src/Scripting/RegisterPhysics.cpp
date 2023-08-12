fbb3fdd2d8e6f6bdd00ce2d47e1a4822aff55bcb
Blake C <blake.caldwell5@gmail.com>
2023-08-12 18:30:54 +0800

    one commit split

---
+++ Core/src/Scripting/RegisterPhysics.cpp
@@ -0,0 +1,17 @@
+#include "LuaAccess.hpp"
+
+#include <reactphysics3d/reactphysics3d.h>
+
+namespace Vakol
+{
+    // TODO remove this later, just used for rp3d vec
+    void RegisterPhysics(sol::state& lua)
+    {
+        // TODO get rid of rp3d vec
+        auto rp3dVec3 = lua.new_usertype<rp3d::Vector3>("PhyVec3"); // need for collider
+        rp3dVec3["x"] = &rp3d::Vector3::x;
+        rp3dVec3["y"] = &rp3d::Vector3::y;
+        rp3dVec3["z"] = &rp3d::Vector3::z;
+    }
+
+}
---
+++ Core/src/Scripting/RegisterTime.cpp
@@ -0,0 +1,15 @@
+#include "LuaAccess.hpp"
+#include "Time/Time.hpp"
+
+namespace Vakol
+{
+    void RegisterTime(sol::state& lua)
+    {
+        auto time_type = lua.new_usertype<Time>("Time");
+        time_type["delta_time"] = &Time::deltaTime;
+        time_type["tick_rate"] = &Time::tickRate;
+        time_type["curr_time"] = &Time::curTime;
+        time_type["prev_time"] = &Time::prevTime;
+        time_type["fps"] = &Time::fps;
+    }
+}
