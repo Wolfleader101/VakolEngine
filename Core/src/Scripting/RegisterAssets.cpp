349aefc4e111abde6aef6daed78784f156eda56b
Caleb <cjmeepers@gmail.com>
2023-08-18 16:08:30 +0800

    Update RegisterAssets.cpp

--- Core/src/Scripting/RegisterAssets.cpp
+++ Core/src/Scripting/RegisterAssets.cpp
@@ -2,12 +2,33 @@
 
 #include "Rendering/Assets/Mesh.hpp"
 #include "Rendering/Assets/Model.hpp"
+#include "Rendering/Assets/Texture.hpp"
+
+#include "AssetLoader/AssetLoader.hpp"
 
 namespace Vakol
 {
-
     void RegisterAssetLoader(sol::state& lua)
     {
+        lua.set_function("load_texture", [](const std::string& path, const bool flip) {
+            if (const auto& texture = std::make_shared<Rendering::Assets::Texture>(AssetLoader::GetTexture(path));
+                texture == nullptr)
+            {
+                return false;
+            }
+
+            return true;
+        });
+
+        lua.set_function("load_model", [](const std::string& path, const float scale = 1.0f) {
+            if (const auto& model = std::make_shared<Rendering::Assets::Model>(AssetLoader::GetModel(path, scale));
+                model == nullptr)
+            {
+                return false;
+            }
+
+            return true;
+        });
     }
 
     void RegisterModel(sol::state& lua)
