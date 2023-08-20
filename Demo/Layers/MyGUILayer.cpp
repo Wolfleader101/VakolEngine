625eb2f6ef864a9d23b09be0db01c39a866717bc
Blake C <blake.caldwell5@gmail.com>
2023-08-20 12:59:24 +0800

    testing

--- Demo/Layers/MyGUILayer.cpp
+++ Demo/Layers/MyGUILayer.cpp
@@ -1,6 +1,8 @@
 #include "MyGUILayer.hpp"
 #include <iostream>
 
+#include <imgui.h>
+
 void MyGUILayer::OnAttach(Vakol::SceneManager* SM)
 {
     m_SceneManager = SM;
@@ -13,6 +15,23 @@ void MyGUILayer::OnDetach()
 void MyGUILayer::OnUpdate()
 {
     std::cout << "MyGUILayer::OnUpdate()" << std::endl;
+
+    // Start a new ImGui frame
+    ImGui::NewFrame();
+
+    // Begin a new window called "Test Window"
+    if (ImGui::Begin("Test Window"))
+    {
+        // Display some static text
+        ImGui::Text("Hello, world!");
+
+        // Create a button
+        if (ImGui::Button("Click Me"))
+        {
+            std::cout << "Button was clicked!" << std::endl;
+        }
+    }
+    ImGui::EndFrame(); // End of "Test Window"
 }
 
 void MyGUILayer::OnEvent(Vakol::Event& event)
