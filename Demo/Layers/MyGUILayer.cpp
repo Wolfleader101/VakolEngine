fbf35046c92a326df2f204b75350aef923bec774
Blake C <blake.caldwell5@gmail.com>
2023-08-20 16:41:13 +0800

    global include exposed

--- Demo/Layers/MyGUILayer.cpp
+++ Demo/Layers/MyGUILayer.cpp
@@ -3,6 +3,8 @@
 
 #include <imgui.h>
 
+#include <SceneManager/SceneManager.hpp>
+
 void MyGUILayer::OnAttach(Vakol::SceneManager* SM)
 {
     m_SceneManager = SM;
@@ -14,7 +16,14 @@ void MyGUILayer::OnDetach()
 
 void MyGUILayer::OnUpdate()
 {
-    std::cout << "MyGUILayer::OnUpdate()" << std::endl;
+    const auto& pos = m_SceneManager->GetActiveScene().GetCamera().GetPos();
+
+    std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+
+    if (ImGui::Begin("Test Window"))
+    {
+        const auto& EL = m_SceneManager->GetActiveScene().GetEntityList();
+    }
 }
 
 void MyGUILayer::OnEvent(Vakol::Event& event)
@@ -29,10 +38,11 @@ void MyGUILayer::OnGUI()
     {
         std::cout << "MyGUILayer::OnGUI()" << std::endl;
         // Display some static text
-        ImGui::Text("Hello, world!");
+        ImGui::Text("Death to Alvaro!");
 
         ImGui::End(); // End of "Test Window"
     }
+}
 }
 
 void MyGUILayer::OnTick()
