17e05901c0869a7c29a6c20939f961babb5db066
Caleb <cjmeepers@gmail.com>
2023-09-10 13:23:06 +0800

    have digi twin on tree stump

--- Demo/scripts/entities/worlds/290_world.lua
+++ Demo/scripts/entities/worlds/290_world.lua
@@ -11,6 +11,11 @@ function init()
 
     props[1]:add_shaded_model("assets/models/290_level/level_opaque_props.fbx", 1.0, "assets/shaders/toon.prog");
     props[2]:add_shaded_model("assets/models/290_level/level_transparent_props.fbx", 1.0, "assets/shaders/toon.prog");
+
+    level:get_transform().pos = Vector3.new(470.0, -340.0, -1487.4);
+
+    props[1]:get_transform().pos = Vector3.new(470.0, -340.0, -1487.4);
+    props[2]:get_transform().pos = Vector3.new(470.0, -340.0, -1487.4);
 end
 
 function update()
