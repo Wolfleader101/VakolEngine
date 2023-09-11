e2670487de3f26dc3936b0c70d4153c94a2d907d
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2023-09-11 19:07:08 +0800

    Props around the scene vak 242 (#107)
    * Weird Scaling of Bench
    
    - Bench is not scaling correctly even though it was created against the building models. Seems to be way smaller than what it should be
    
    * Update digital_twin.lua
    
    * Adding Models + Fixed Scaling Function
    
    - Adding models to the scene
    - Fixed the scaling function in Math.cpp. Was just a copy of the translation function, now is set to the correct format for scaling (Used https://www.brainvoyager.com/bv/doc/UsersGuide/CoordsAndTransforms/SpatialTransformationMatrices.html to reference what it should look like)
    
    * Update Math.cpp
    
    - Forgot to remove the old return value
    
    * Update Math.cpp
    
    - Formatting
    
    * Remove FBX files from tracking as they are quite large
    
    * Update .gitignore to exclude Demo/assets/
    
    * Fixed Scaling Problem with Models
    
    - Fixed the weird scaling problem with models. This was due to an error on the Maya side of things where the Transforms weren't being frozen (Reset back to 1 for Scale/Translate and 0 for rotations). These were getting added on top of what we had, so now we don't need to scale anything for the Digital Twin
    
    * Update .gitignore
    
    - Updated so instead of ignoring the whole folder, we are just ignoring Textures and Models as they are the largest
    
    * Update digital_twin.lua
    
    - Starting to place objects (Rotations are not being applied for some reason)
    
    * Update digital_twin.lua
    
    - Added loops to create all of the entities needed for the scene
    
    * Positioning Objects + Fixed Rotations
    
    - Positioning the props around the scene (Using loops so I don't have huge amounts of lines)
    - Fixed the issue with rotations not being applied (Thanks James)
    
    * Added Props + Added Speed Up Ability for Noclip
    
    - Added all of the props to the scene (That GUI Menu really helps to position everything and get the values I need)
    - Added the ability to hold Left Shift while Noclipping to speed yourself up (It was taking forever to move around the scene so I added this so I could check areas faster)
    
    * Updated Colliders + Added back FBX Files
    
    - Updated the adding of the rigidbodies and colliders so they were correctly added to the entity and not made into their own entities (Thanks to James for pointing it out)
    - Added back the FBX files as team members were having trouble with them being missing
    
    * Update digital_twin.lua
    
    - Colliders have all been added (NOTE: Framerate dips from 600+ FPS down to around 30 FPS with debug rendering turned on)
    
    * Moved Prop Creation to Separate Script
    
    - Moved prop creation to a separate script so it wasn't clogging up the main script for creating the digital twin geometry (Cheers Blake for the suggestion)
    
    * Update Application.cpp
    
    - Clang Formatting
    
    * Moved Scripts to 'worlds' folder
    
    - Moved the Digital Twin scripts to the 'worlds' folder

--- Core/src/Math/Math.cpp
+++ Core/src/Math/Math.cpp
@@ -150,7 +150,7 @@ namespace Vakol::Math
 
     Mat4 Scale(const Vec3& scale)
     {
-        return Mat4(1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, scale.x, scale.y, scale.z,
+        return Mat4(scale.x, 0.0f, 0.0f, 0.0f, 0.0f, scale.y, 0.0f, 0.0f, 0.0f, 0.0f, scale.z, 0.0f, 0.0f, 0.0f, 0.0f,
                     1.0f);
     }
 
