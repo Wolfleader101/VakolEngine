2643353d36b741196834ff2017f866f9a49827f1
Wolfleader <25214835+Wolfleader101@users.noreply.github.com>
2023-10-11 11:46:03 +0800

    Affordances in lua vak 275 (#115)
    * attach multiple scripts to an entity
    
    * moved vars

--- Core/src/Scripting/include/Scripting/ScriptTypes.hpp
+++ Core/src/Scripting/include/Scripting/ScriptTypes.hpp
@@ -3,21 +3,35 @@
 #define SOL_ALL_SAFETIES_ON 1
 #include <sol/sol.hpp>
 
+#include <vector>
+
 namespace Vakol
 {
-    /**
-     * @brief Wrapper around sol environment to have a reference to an active script
-     *
-     */
-    struct LuaScript
-    {
-        sol::environment env;
-        std::string path;
-    };
+    using LuaEnvironment = sol::environment;
 
     using LuaType = sol::object;
     using LuaTable = sol::table;
     using LuaFunction = sol::protected_function;
 
     using LuaState = sol::state;
+    /**
+     * @brief Wrapper around sol environment to have a reference to an active script
+     *
+     */
+    struct LuaScript
+    {
+        std::string name = "";
+        LuaEnvironment env = {};
+        std::string path = "";
+    };
+
+    /**
+     * @brief script compoenent for an entity
+     *
+     */
+    struct ScriptComp
+    {
+        std::vector<LuaScript> scripts;
+    };
+
 } // namespace Vakol
