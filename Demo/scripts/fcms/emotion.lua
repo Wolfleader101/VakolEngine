816ed2359a65fd44e6cba75212a61d5325fa0ec3
Blake C <blake.caldwell5@gmail.com>
2023-10-12 15:42:27 +0800

    finished script

--- Demo/scripts/fcms/emotion.lua
+++ Demo/scripts/fcms/emotion.lua
@@ -1,15 +1,16 @@
 
-ANGER <const> = 0;
-ANTICIPATION <const> = 1;
-JOY <const> = 2;
-TRUST <const> = 3;
-FEAR <const> = 4;
-SURPRISE <const> = 5;
-SADNESS <const> = 6;
-DISGUST <const> = 7;
+ANGER = 1;
+ANTICIPATION = 2;
+JOY = 3;
+TRUST = 4;
+FEAR = 5;
+SURPRISE = 6;
+SADNESS = 7;
+DISGUST = 8;
 
 
-local emotion_concepts = { 0  ,  0  , 0  , 0  , 0  , 0  , 0  , 0   };
+local emotion_names = {"Anger", "Anticipation", "Joy", "Trust", "Fear", "Surprise", "Sadness", "Disgust"};
+local emotion_concepts = { -0.4  ,     0  ,    0.96  ,   0.5  ,  -0.9  ,    0  ,      0.1  ,     -0.9   };
 
 local decays           = { 0.05, 0.03, 0.1, 0.001, 0.01, 0.15, 0.025, 0.01 };
 
@@ -24,6 +25,15 @@ local weights = {
     {  0.5 ,  0    , -0.8  , -0.1  ,  0.2  ,  0    ,  0    ,  0.15 },
     {  0.1 ,  0.2  , -0.45 , -0.8  ,  0.1  ,  0    ,  0.1  ,  0    }
 }
+
+function print_emotions()
+    
+    
+    for i = 1, 8 do
+        io.write(emotion_names[i] .. ": " .. emotion_concepts[i] .. " | ")
+    end
+end
+
 
 
 function get_emotion(emotion_index)
@@ -46,10 +56,34 @@ function set_emotion(emotion_index, value)
 
 end
 
+local function tanh(x) --activation function. bounds between -1 and 1
+    local e_pos = math.exp(x)     
+    local e_neg = math.exp(-x)    
+    return (e_pos - e_neg) / (e_pos + e_neg)
+end
+
 local function iterate()
+
+    new_concepts = {};
+
+    for i = 1, 8 do
+        sum = 0;
+
+        for j = 1, 8 do
+            sum = sum + emotion_concepts[j] * weights[j][i] -- accidentally made transpose so reverse indexing gg lmao
+        end
+        
+        new_concepts[i] = emotion_concepts[i] + sum - decays[i];
+    end
+
+    for i = 1, 8 do
+        emotion_concepts[i] = new_concepts[i]
 
 end
 
+
+
+-- ================== VAKOL FUNCS
 
 function init()
 
@@ -62,7 +96,7 @@ end
 
 
 function tick()
-
+    iterate();
 end
 
 function phys_update()
