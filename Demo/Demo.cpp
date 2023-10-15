ffbd3df68f92b7968849cdc6999123132695acc2
Blake C <blake.caldwell5@gmail.com>
2023-10-15 14:15:18 +0800

    Fcm vak 278 (#116)
    * ended up researching emotion models
    
    Do have a plan now
    
    * weights
    
    * finished script
    
    * lmao busted
    
    * normalize
    
    * tweaking
    
    * symmetrical matrix
    
    * closer, added L2 normalization
    
    * kind of good?
    
    * FCM turned into layer work gotem
    
    * fetching emotion data
    
    * need to fix crashing and update logic
    
    * close
    
    * added Demo implot
    
    * moved cmake external to implot
    
    they didnt have cmake I had to supply one
    
    * removed printing for pr

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -1,6 +1,8 @@
 ﻿#include <memory>
 
+#include "Layers/EmotionLayer.hpp"
 #include "Layers/MyGUILayer.hpp"
+
 #include "Vakol/core.hpp"
 
 int main()
@@ -11,6 +13,8 @@ int main()
     Vakol::Application app = Vakol::Application();
 
     app.PushLayer(std::make_shared<MyGUILayer>(app), Vakol::LayerFlags::UPDATES | Vakol::LayerFlags::EVENTS);
+    app.PushLayer(std::make_shared<EmotionLayer>(app),
+                  Vakol::LayerFlags::UPDATES | Vakol::LayerFlags::EVENTS | Vakol::LayerFlags::TICKS);
 
     app.Run();
 
