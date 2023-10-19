e0ac42fbfaf2c5839aec504468bb0cbb62a0516d
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2023-10-19 21:27:13 +0800

    Update bench.lua
    - Still not sure about what is causing the issue. Possibly has to do with the Rigidbody

--- Demo/scripts/entities/objects/bench.lua
+++ Demo/scripts/entities/objects/bench.lua
@@ -2,10 +2,7 @@ function init()
     entity:add_model("assets/models/imported/OpenGameArt/Teh_Bucket/Bench/bench.fbx", 0.02);
     
     local rb = entity:add_rigid();
-	rb.mass = 1;
-    rb.bounciness = 0.3;
-    rb.type = BodyType.Dynamic;
-    rb.hasGravity = true;
+	rb.type = BodyType.Dynamic;
     entity:add_box_collider(Vector3.new(2.64, 1.016, 2.158));
     
     entity:add_script("affordance", "components/affordance.lua")
