d4ed1528e6904e4ae484d47f00e42214020b338b
Blake C <blake.caldwell5@gmail.com>
2023-10-22 13:38:09 +0800

    think we sweet

--- Demo/scripts/components/navigation.lua
+++ Demo/scripts/components/navigation.lua
@@ -1,6 +1,5 @@
-
-NAVIGATE = false;
 DESTINATION = Vector3.new();
+local state = "wander"; -- states: "navigate", "wander", "wait"
 local trans = Transform.new();
 local dir = Vector3.new();
 
@@ -16,73 +15,114 @@ local function distance()
     return (DESTINATION - trans.pos):magnitude();
 end
 
-function init()
-	
+function set_state(new_state)
+    state = new_state;
 end
 
-local WANDER_TICKS = 300; --5 seconds
-local wanderCount = 0;
+function init()
 
+end
+
+local WANDER_TICKS = 300; -- 5 seconds
+local wanderCount = 0;
+local wanderChanges = 0; -- used to switch to wait state
+
+local WAIT_TICKS = 600; -- 10 seconds 
+local waitCount = 0;
+
+local DIR_CHANGE_COOLDOWN = 300; -- 1 second
+local dirChangeCounter = 0;
+
+local currentRotation = 0;
+local targetRotation = 0;
+local ROTATION_LERP_SPEED = 0.05; -- lerping bruvva
 
 function tick()
 
-    if NAVIGATE then
+    print(state);
+
+    if state == "navigate" then
 
         trans = entity:get_transform();
         dir = get_direction();
-        targetRotation = math.atan(dir.x, dir.z) * (180 / math.pi);  
-        entity:get_transform().rot.y = targetRotation; 
+        targetRotation = math.atan(dir.x, dir.z) * (180 / math.pi);
+        entity:get_transform().rot.y = targetRotation;
 
         dist = distance();
 
         if dist < MAX_DIST then
-            NAVIGATE = false;
+            state = "wander";
             rb = entity:get_rigid();
-            rb.linearVelocity = Vector3.new(0,0,0);
+            rb.linearVelocity = Vector3.new(0, 0, 0);
         else
 
             info = RayCastHitInfo.new();
-            
 
-            local rayOrigin = trans.pos + Vector3.new(0, 5, 0);  -- so we dont cast from inside agent collider
+            local rayOrigin = trans.pos + Vector3.new(0, 5, 0); -- so we dont cast from inside agent collider
 
-            if scene:raycast(rayOrigin, dir, 0.25 , info) and info.distance < CHECK_DIST then
+            if scene:raycast(rayOrigin, dir, 0.25, info) and info.distance < CHECK_DIST then
                 -- get normal and change dir to follow it. Not the best but will do the job
                 local reflectDir = dir - 2 * (dir:dot(info.normal)) * info.normal;
                 dir = reflectDir:normalize()
             end
         end
-    else --wander state
-        
-        
-        if wanderCount >= WANDER_TICKS then
-            local randomAngle = math.random() * 2 * math.pi;
-            dir = Vector3.new(math.cos(randomAngle), 0, math.sin(randomAngle));
-            wanderCount = 0;
+
+    elseif state == "wait" then
+
+        if waitCount >= WAIT_TICKS then
+            state = "wander"; -- Transition back to wander after waiting
+            waitCount = 0;
         else
-            wanderCount = wanderCount + 1;    
+            waitCount = waitCount + 1;
         end
-    
+
+    else -- wander state
+
+        if dirChangeCounter > 0 then
+            dirChangeCounter = dirChangeCounter - 1;
+        end
+
+        if wanderCount == 0 or wanderCount >= WANDER_TICKS then
+            if dirChangeCounter == 0 then
+                local randomAngle = math.random() * 2 * math.pi;
+                local newDir = Vector3.new(math.cos(randomAngle), 0, math.sin(randomAngle));
+                dir = dir * 0.8 + newDir * 0.2; -- Interpolate to smooth direction change
+                wanderCount = 0;
+                dirChangeCounter = DIR_CHANGE_COOLDOWN;
+                wanderChanges = wanderChanges + 1;
+            end
+        else
+            wanderCount = wanderCount + 1;
+        end
+
         targetRotation = math.atan(dir.x, dir.z) * (180 / math.pi);
         entity:get_transform().rot.y = targetRotation;
-    
+
         info = RayCastHitInfo.new();
-    
+
         -- If there's an obstacle in the wander direction, slide along it
-        if scene:raycast(trans.pos, dir, CHECK_DIST, info) and info.hit then
+        if scene:raycast(trans.pos, dir, CHECK_DIST, info) and info.hit and dirChangeCounter == 0 then
             slideDir = Vector3.new(-info.normal.z, 0, info.normal.x);
-            dir = slideDir;
+            dir = dir * 0.8 + slideDir * 0.2; -- Interpolate to smooth direction change
+            dirChangeCounter = DIR_CHANGE_COOLDOWN;
+        end
+
+        targetRotation = math.atan(dir.x, dir.z) * (180 / math.pi);
+
+        if wanderChanges == 5 then
+            state = "wait";
+            wanderChanges = 0;
         end
 
     end
 
-        rb = entity:get_rigid();
-        vel = Vector2.new(rb.linearVelocity.x, rb.linearVelocity.z):magnitude();
+    currentRotation = currentRotation + (targetRotation - currentRotation) * ROTATION_LERP_SPEED;
+    entity:get_transform().rot.y = currentRotation;
 
-        if vel < MAX_SPEED then
-            rb:apply_impulse(dir);
-        end
+    rb = entity:get_rigid();
+    vel = Vector2.new(rb.linearVelocity.x, rb.linearVelocity.z):magnitude();
 
-        str = dir.x .. " " .. dir.z;
-        print(str);
+    if vel < MAX_SPEED and state ~= "wait" then
+        rb:apply_impulse(dir);
+    end
 end
--- Demo/scripts/entities/test/nav_test.lua
+++ Demo/scripts/entities/test/nav_test.lua
@@ -1,11 +1,9 @@
-
-
 function init()
-    
+
     entity:add_model("assets/models/agents/bob.glb", 1.0e-02);
-    entity:get_transform().pos = Vector3.new(-40, 5, -20); --shouldnt be here 
+    entity:get_transform().pos = Vector3.new(-40, 5, -40); -- shouldnt be here 
     local rb = entity:add_rigid();
-	rb.mass = 1;
+    rb.mass = 1;
     entity:add_box_collider(Vector3.new(1.0, 1.0, 1.0));
 
     entity:add_script("emotions", "components/emotion.lua");
@@ -15,10 +13,10 @@ function init()
 
     target = Vector3.new(0, 0, 0);
 
-    nav.NAVIGATE = true
     nav.DESTINATION.x = target.x
     nav.DESTINATION.y = target.y
     nav.DESTINATION.z = target.z
-    	
-    
+
+    nav.set_state("navigate");
+
 end
