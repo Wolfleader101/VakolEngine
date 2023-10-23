2c5c6b1c495281d20850b0ebeedd8999ccf4d9d1
Caleb <cjmeepers@gmail.com>
2023-10-24 00:50:49 +0800

    yeah i wish

--- Demo/scripts/components/interactable.lua
+++ Demo/scripts/components/interactable.lua
@@ -13,8 +13,10 @@ INTERACTABLE =
 			self.is_activated = true;
 
 			self.interacting_agent = parent;
-			
-			print(self.is_throwable);
+
+			if (self.is_throwable and entity:get_script("throwable") == nil) then
+				entity:add_script("throwable", "components/throwable.lua");
+			end
 		else if (self.is_activated and self.interact_count > 0) then
 				self.interact_count = self.interact_count - 1;
 				self.is_activated = false;
