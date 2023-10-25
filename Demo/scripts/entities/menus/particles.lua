43c868f77cd173e90f16665b2374da7e02bfc083
Blake C <blake.caldwell5@gmail.com>
2023-10-26 00:30:03 +0800

    Wacky

---
+++ Demo/scripts/entities/menus/particles.lua
@@ -0,0 +1,62 @@
+local particles = {}
+local index = 1;
+local MAX_PARTICLE <const> = 100;
+
+local y_storage;
+
+function init()
+    local tag = entity:get_tag();
+    y_storage = randomFloat(-1000, -100);
+    for i = 1, MAX_PARTICLE do
+        particle = scene:create_entity(" particle " .. i, "");
+
+        local particle_trans = particle:get_transform();
+        particle_trans.pos = Vector3.new(i, y_storage, i);
+        -- particle_trans.scale = Vector3.new(0.05, 0.05, 0.05);
+        particle_trans.scale = Vector3.new(0.2, 0.2, 0.2);
+
+        local model = particle:add_model("coreAssets/models/sphere.obj", 1);
+        local mesh = model:get_mesh(0);
+
+        mesh.material:set_diffuse_color(Vector4.new(randomFloat(0, 1), randomFloat(0, 1), randomFloat(0, 1), 1.0));
+
+        local rb = particle:add_rigid();
+        particle:add_sphere_collider(0.025);
+        particles[i] = particle;
+        rb.hasGravity = false;
+        rb.is_sleeping = true;
+    end
+end
+
+function randomFloat(lower, greater)
+    return lower + math.random() * (greater - lower);
+end
+
+function shoot_particle(pos, forward)
+    index = (index % MAX_PARTICLE) + 1;
+    particle = particles[index];
+    particle:get_transform().pos = pos;
+
+    rb = particle:get_rigid();
+
+    randVec = Vector3.new(randomFloat(0, 3), randomFloat(0, 3), randomFloat(0, 3));
+    rb:apply_impulse(forward + randVec);
+end
+
+function clean()
+    for i = 1, MAX_PARTICLE do
+        trans = particles[i]:get_transform();
+        trans.pos = Vector3.new(i, y_storage, i);
+
+        rb = particles[i]:get_rigid();
+        rb.hasGravity = false;
+        rb.is_sleeping = false;
+    end
+end
+
+local pos = Vector3.new(0, 0, -50);
+local forward = Vector3.new(0, 0, 1);
+
+function tick()
+    shoot_particle(pos, forward)
+end
