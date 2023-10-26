8046ebc5c6d09c082af4989d95076a8768363fb0
Caleb <cjmeepers@gmail.com>
2023-10-26 23:18:11 +0800

    hi

--- Demo/scripts/main.lua
+++ Demo/scripts/main.lua
@@ -1,5 +1,5 @@
---add_scene("anti_james", "scenes/anti_james.lua");
---add_scene("test menu", "scenes/start_scene.lua");
+add_scene("anti_james", "scenes/anti_james.lua");
+add_scene("test menu", "scenes/start_scene.lua");
 
 add_scene("sandbox", "scenes/sandbox.lua");
 
