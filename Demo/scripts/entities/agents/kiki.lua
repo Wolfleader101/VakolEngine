042cd0b38f3c2373aad2152e4a645dc814e66892
Caleb <cjmeepers@gmail.com>
2023-10-27 20:32:03 +0800

    yes

--- Demo/scripts/entities/agents/kiki.lua
+++ Demo/scripts/entities/agents/kiki.lua
@@ -3,9 +3,11 @@ local nav = nil;
 
 local target = Vector3.new();
 
+local eye_material = nil;
+
 function init()
     local model = entity:add_model("assets/models/ai/kiki/kiki.obj", 0.5);
-    local eye_material = model:get_mesh(1).material;
+    eye_material = model:get_mesh(1).material;
 
     entity:get_transform().pos = Vector3.new(0.0, 5.0, 0.0);
 
@@ -32,7 +34,7 @@ function init()
 
     nav.set_state("wander");
 
-    eye_material:set_uv_offset(Vector2.new(1.0, 0.5));
+    eye_material:set_uv_offset(Vector2.new(0.0, 0.0));
     eye_material:set_opacity(0.0);
     eye_material:use_lighting(false);
 end
@@ -40,6 +42,7 @@ end
 function tick()
     
 end
+
 
 function update()
     
